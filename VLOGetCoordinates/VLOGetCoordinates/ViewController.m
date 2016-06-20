03e1377e278e24f0ff5f89d76a514ff223e54551
minsun <minsun_09@hanmail.net>
2016-06-20 16:18:10 +0900

    입력값 타입을 Location에서 VLOPlace로 변경

--- VLOGetCoordinates/VLOGetCoordinates/ViewController.m
+++ VLOGetCoordinates/VLOGetCoordinates/ViewController.m
@@ -12,6 +12,7 @@
 #import "VLOLocationCoordinate.h"
 #import "getCoordinates.h"
 #import "Marker.h"
+#import "VLOPlace.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,11 @@
     
     NSInteger cnt;
     NSInteger i;
+    
+    VLOPlace * vp=[[VLOPlace alloc]init];
+    VLOPlace * vp2=[[VLOPlace alloc]init];
+    VLOPlace * vp3=[[VLOPlace alloc]init];
+    
     
     VLOLocationCoordinate * vc1=[[VLOLocationCoordinate alloc]init];
     VLOLocationCoordinate * vc2=[[VLOLocationCoordinate alloc]init];
@@ -38,10 +44,17 @@
     [vc3 setLatitude:[NSNumber numberWithDouble:52.9541053]];
     [vc3 setLongitude:[NSNumber numberWithDouble:-1.2401013]];
     
+    vp.coordinates=vc1;
+    vp.name=@"Madrid";
+    vp2.coordinates=vc2;
+    vp2.name=@"Barcelona";
+    vp3.coordinates=vc3;
+    vp3.name=@"Seoul";
+    
     NSMutableArray * location_list=[NSMutableArray array];
-    [location_list addObject:vc1];
-    [location_list addObject:vc2];
-    [location_list addObject:vc3];
+    [location_list addObject:vp];
+    [location_list addObject:vp2];
+    [location_list addObject:vp3];
     
     
     
@@ -53,11 +66,16 @@
     
     cnt=[print_coordinates count];
     
+
+    
     for(i=0;i<cnt;i++)
     {
         print_marker=[print_coordinates objectAtIndex:i];
         UIImageView *iv=[[UIImageView alloc]initWithFrame:CGRectMake(print_marker.x, print_marker.y, 15, 20)];
         iv.image=[UIImage imageNamed:@"location-placemark-gradient.png"];
+        
+        NSLog(@"%@",print_marker.name);
+
         [self.view addSubview:iv];
     }
     
