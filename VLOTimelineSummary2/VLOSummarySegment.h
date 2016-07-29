e0f700324f67dd2aa2e4721d9cde76fcf4149728
seongminnpark <seongmi1@andrew.cmu.edu>
2016-07-29 10:15:23 +0900

    마커, 세그먼트 인터페이스 추가

--- VLOTimelineSummary2/VLOSummarySegment.h
+++ VLOTimelineSummary2/VLOSummarySegment.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIkit/UIkit.h>
+#import "VLOSummaryMarker.h"
+
+#define LEFT_TO_RIGHT = 0;
+#define RIGHT_TO_LEFT = 1;
 
 @interface VLOSummarySegment : NSObject
+
+
+@property (strong, nonatomic) VLOSummaryMarker *fromMarker;
+@property (strong, nonatomic) VLOSummaryMarker *toMarker;
+@property () BOOL curved;
+@property () NSInteger direction;
+
+- (id) initFrom:(VLOSummaryMarker *)fromMarker to:(VLOSummaryMarker *)toMarker;
+- (void) setLine:(UIImage *)lineImage;
+- (void) setLineContent:(UIImage *)contentImage;
+- (UIView *) getDrawableView;
 
 @end
