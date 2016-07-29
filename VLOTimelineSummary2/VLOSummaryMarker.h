f9a884d28b95c9ba55398c98f3e22fe951104061
seongminnpark <seongmi1@andrew.cmu.edu>
2016-07-29 09:40:12 +0900

    써머리2 폴더 추가

---
+++ VLOTimelineSummary2/VLOSummaryMarker.h
@@ -0,0 +1,13 @@
+//
+//  VLOSummaryMarker.h
+//  Volo
+//
+//  Created by Seongmin on 7/29/16.
+//  Copyright © 2016 SK Planet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VLOSummaryMarker : NSObject
+
+@end
---
+++ VLOTimelineSummary2/VLOSummarySegment.h
@@ -0,0 +1,13 @@
+//
+//  VLOSummarySegment.h
+//  Volo
+//
+//  Created by Seongmin on 7/29/16.
+//  Copyright © 2016 SK Planet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VLOSummarySegment : NSObject
+
+@end
